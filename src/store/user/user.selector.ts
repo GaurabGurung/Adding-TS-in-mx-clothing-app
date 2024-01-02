1b55970ab097de9332c7308d99e7b3abf88d8d05
Gaurab Gurung <gaurabgurung181@gmail.com>
2024-01-02 18:44:05 +1100

    finsihed Typing our store.js file

--- src/store/user/user.selector.ts
+++ src/store/user/user.selector.ts
@@ -1,8 +1,9 @@
+import { RootState } from "../store";
 import { UserState } from "./user.reducer";
 
 import { createSelector } from "reselect";
 
-export const selectUserReducer = (state): UserState => state.user;
+export const selectUserReducer = (state: RootState): UserState => state.user;
 
 export const selectCurrentUser = createSelector(
   selectUserReducer,
